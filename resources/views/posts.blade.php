d3a11c0b6781fdb1050ddee97a336128da376baf
JakubVondra <jakubvond@seznam.cz>
2023-06-06 15:01:23 +0200

    Use the filesystem class to read a directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,28 +9,10 @@
     <script src="/app.js"></script>
 </head>
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates doloremque amet dicta voluptatibus eveniet, soluta aliquam cupiditate sunt quod aperiam minus reprehenderit numquam alias accusantium error quaerat eaque sit nam.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates doloremque amet dicta voluptatibus eveniet, soluta aliquam cupiditate sunt quod aperiam minus reprehenderit numquam alias accusantium error quaerat eaque sit nam.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates doloremque amet dicta voluptatibus eveniet, soluta aliquam cupiditate sunt quod aperiam minus reprehenderit numquam alias accusantium error quaerat eaque sit nam.
-        </p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
 </html>
