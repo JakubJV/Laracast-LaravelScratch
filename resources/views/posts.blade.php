c7a126621e285c6e8c6adac47e13e931e2d816d0
JakubVondra <jakubvond@seznam.cz>
2023-06-06 12:55:12 +0200

    Created route for single post

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates doloremque amet dicta voluptatibus eveniet, soluta aliquam cupiditate sunt quod aperiam minus reprehenderit numquam alias accusantium error quaerat eaque sit nam.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates doloremque amet dicta voluptatibus eveniet, soluta aliquam cupiditate sunt quod aperiam minus reprehenderit numquam alias accusantium error quaerat eaque sit nam.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates doloremque amet dicta voluptatibus eveniet, soluta aliquam cupiditate sunt quod aperiam minus reprehenderit numquam alias accusantium error quaerat eaque sit nam.
+        </p>
+    </article>
+</body>
+</html>
