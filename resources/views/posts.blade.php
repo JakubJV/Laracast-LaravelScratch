6954d3caef80c02ae2a29bccecc919fedc9937ba
JakubVondra <jakubvond@seznam.cz>
2023-06-07 15:48:48 +0200

    Eloquent updates and html escaping

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,16 +3,16 @@
     
 
     @foreach ($posts as $post)
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->id }}">
-                {{ $post -> title}}
-            </a>
-        </h1>
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->id }}">
+                    {{ $post -> title}}
+                </a>
+            </h1>
 
-        <div>
-            {{ $post->excerpt }}
-        </div>
-    </article>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
     @endforeach
 @endsection
