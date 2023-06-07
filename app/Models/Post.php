51b15361073fd6e4e512685a8efebf31e74dcd1f
JakubVondra <jakubvond@seznam.cz>
2023-06-07 10:58:48 +0200

    Composer Package for Post Metadata

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,26 +3,52 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use PhpParser\Node\Expr\StaticCall;
 
 class Post
 {
+
+    public $title;
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function all()
     {
-        $files =  File::files(resource_path("posts/"));
+        return collect(File::files(resource_path("posts")))
 
-        return array_map(function ($file) {
-            return $file->getContents();
-        }, $files);
+            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+
+            ->map(fn ($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ))
+            ->sortBy('date');
     }
 
     public static function find($slug)
     {
-        base_path();
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
+        // of all the blog posts, find the one with a slug that matches the one that was requested
 
-        return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 }
