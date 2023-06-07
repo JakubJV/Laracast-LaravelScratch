a36f9e17551e0fd9dcff5e4e058dd8c32da26746
JakubVondra <jakubvond@seznam.cz>
2023-06-07 12:31:54 +0200

    Blade directives Two ways

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
-<body>
+@extends('components.layout')
+
+@section('content')
     <article>
         <h1>{{ $post->title }}</h1>
 
@@ -18,4 +10,4 @@
     </article>
 
     <a href="/">Go Back</a>
-</body>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
-<body>
-    @foreach ($posts as $post)
-        <article class="{{ $loop->even ? 'foobar' : ''}}">
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post -> title}}
-                </a>
-            </h1>
+@extends('components.layout')
+@section('content')
+    
 
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
+    @foreach ($posts as $post)
+    <article class="{{ $loop->even ? 'foobar' : ''}}">
+        <h1>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post -> title}}
+            </a>
+        </h1>
+
+        <div>
+            {{ $post->excerpt }}
+        </div>
+    </article>
     @endforeach
-</body>
-</html>
+@endsection
