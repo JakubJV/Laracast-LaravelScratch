d76cb08ec99a6a360f42189887aff3a5ff6c6deb
JakubVondra <jakubvond@seznam.cz>
2023-06-07 11:42:52 +0200

    Sorting and Caching

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -31,18 +31,20 @@ class Post
 
     public static function all()
     {
-        return collect(File::files(resource_path("posts")))
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts")))
 
-            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
 
-            ->map(fn ($document) => new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            ))
-            ->sortBy('date');
+                ->map(fn ($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+                ->sortByDesc('date');
+        });
     }
 
     public static function find($slug)
